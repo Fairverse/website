68dff4fe1ca8b39499a7253c185778b133b10673
missreyyo <reyhanahsen7@gmail.com>
2022-07-17 11:04:05 +0300

    social media icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,6 @@
 import React from "react";
 import InstagramIcon from "@material-ui/icons/Instagram";
 import TwitterIcon from "@material-ui/icons/Twitter";
-
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import "../styles/Footer.css";
 
@@ -9,8 +8,13 @@ function Footer() {
   return (
     <div className="footer">
       <div className="socialMedia">
-   
-        <InstagramIcon  /> <TwitterIcon />  <LinkedInIcon />
+      <a href="https://www.instagram.com/mskuresearchgroup/">
+      <InstagramIcon  /> </a>
+      <a href="https://twitter.com/MSKUMvrg">
+      <TwitterIcon />  </a>
+      <a href="https://www.linkedin.com/company/msku-metaverse-research-group/">
+      <LinkedInIcon />  </a>
+          
       </div>
       <p> &copy; 2022 fairverse.ceng.mu.edu.tr</p>
     </div>
