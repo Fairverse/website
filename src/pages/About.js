d135900269820987e46ed9fe31da538f96354a51
missreyyo <reyhanahsen7@gmail.com>
2022-07-18 11:45:02 +0300

    kullanılan teknolojiler- hakkında sayfası

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,9 @@
 import React from "react";
-
+import ekip from "../assets/ekip.png";
+import metamask from "../assets/metamask.jpg";
+import ready from  "../assets/ready-player.jpg";
+import reactjs from "../assets/react-js.jpg";
+import unity from "../assets/unity.png";
 import "../styles/About.css";
 function About() {
   return (
@@ -7,26 +11,33 @@ function About() {
       <div  className="aboutTop" 
       ></div>
       <div className="aboutBottom">
-        <h1> Hakkımızda</h1>   
+        <h1 className="kullanılan"> Hakkımızda</h1>   
         <h2 className="aboutTop">
           Biz Kimiz?
         </h2>
-        <h3>Bizler Muğla Sıtkı Koçman Üniversitesi Bilgisayar Mühendisliği öğrencileri olarak halihazırda yazılım dillerini öğrenen ve öğrenmeye devam eden, teknoloji meraklısı gençleriz. Yeni teknolojilere açığız ve geleceğe yön vermek istiyoruz. Okulumuzda bulunan Metaverse Araştırma Grubu (MvRG) ile son günlerde revaçta olan metaverse konusunda kendimizi geliştiriyoruz. Takım olarak amacımız; metaverse kavramını kullanarak hayatımızı kolaylaştıracak teknolojik ve yenilikçi içeriklere sahip projelere imza atmaktır. Bu açıdan sosyal faydayı gözeten ve engelli bireylerin erişebilirliklerini arttırabilecek bir proje fikri bizi heyecanlandırıyor.</h3>
-        
+        <h3 className="bilgi">Bizler Muğla Sıtkı Koçman Üniversitesi Bilgisayar Mühendisliği öğrencileri olarak halihazırda yazılım dillerini öğrenen ve öğrenmeye devam eden, teknoloji meraklısı gençleriz. Yeni teknolojilere açığız ve geleceğe yön vermek istiyoruz. Okulumuzda bulunan Metaverse Araştırma Grubu (MvRG) ile son günlerde revaçta olan metaverse konusunda kendimizi geliştiriyoruz. Takım olarak amacımız; metaverse kavramını kullanarak hayatımızı kolaylaştıracak teknolojik ve yenilikçi içeriklere sahip projelere imza atmaktır. Bu açıdan sosyal faydayı gözeten ve engelli bireylerin erişebilirliklerini arttırabilecek bir proje fikri bizi heyecanlandırıyor.</h3>       
       </div>
       <div>
-      <h2 className="aboutTop">
-          Hedef Kitlemiz
-        </h2>
-        <h3>Projemizdeki hedef kitle; fiziksel engele sahip bireylerdir. Kullanıcılardan sesli komutları alıp
-görüntü ve ses komutları olarak geri dönüt vereceğimiz için bireyin konuşma ve görme
-yeteneklerini kaybetmemiş olması gerekmektedir. İşitme engelli bireyler de görme
-duyusundan faydalanarak sistemden yararlanabilecektir. Bu tür engelli dostlarımızla prototipin
-denenmesi hedeflenmektedir.</h3>
-
+        <img src={ekip} alt="" className="ekip"></img>
+        <h1 className="kullanılan">Kullanılan Teknolojiler</h1>
       </div>
-
-
+    
+      <div>
+        <h2>Ready Player Me</h2>
+        <a href="https://readyplayer.me/tr"><img src={ready} alt="" className="fotolar"></img></a>
+      </div>
+      <div>
+        <h2>MetaMask</h2>
+        <a href="https://metamask.io/"> <img src={metamask} alt="" className="fotolar"></img></a>
+      </div>
+      <div>
+        <h2>React JS</h2>
+        <a href="https://tr.reactjs.org/"> <img src={reactjs} alt="" className="fotolar"></img></a>
+      </div>
+      <div>
+        <h2>Unity</h2>
+        <a href="https://unity.com/"> <img src={unity} alt="" className="fotolar"></img></a>
+      </div>
     </div>
   );
 }
